4ea4a3346dd1789c4ccfa716f1fbec777425b3ae
Zhong Zhenyu <nczzy1997@gmail.com>
2021-11-08 21:22:19 +0800

    UI improvements

--- views/message/index.php
+++ views/message/index.php
@@ -7,29 +7,32 @@ use yii\helpers\Markdown;
 /* @var $messages array */
 /* @var $changeLogs array */
 
-$this->title = '公告';
+$this->title = '平台公告';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="message-index">
 
-    <h1 class="mb-4"><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1>
 
     <?php foreach ($messages as $message): ?>
 
-        <div class="alert alert-<?= $message->mode ?>">
-            <?= $message->text ?>
+        <div class="card bg-<?= $message->mode ?> text-white mb-2">
+            <div class="card-body">
+                <?= $message->text ?>
+            </div>
         </div>
 
     <?php endforeach ?>
 
-    <h2 class="mt-4 mb-4">更新记录</h2>
+    <h2>更新记录</h2>
 
     <?php foreach ($changeLogs as $changeLog): ?>
 
-        <div class="card bg-light mb-3">
+        <div class="card bg-light mb-2">
+            <div class="card-header">
+                <h4 class="mb-0"><?= '版本 ' . $changeLog->version ?></h4>
+            </div>
             <div class="card-body">
-                <?= Markdown::process('### 版本 ' . $changeLog->version) ?>
-                <hr/>
                 <p>日期 <?= date('Y年m月d日', $changeLog->created_at) ?></p>
                 <?= Markdown::process($changeLog->text) ?>
             </div>
--- views/site/index.php
+++ views/site/index.php
@@ -8,7 +8,7 @@ $this->title = Yii::$app->name;
 <div class="site-index">
 
     <div class="jumbotron text-center">
-        <?= Html::img('@web/nankai-logo.svg', ['alt' => '南开大学', 'class' => 'jumbotron-logo']) ?>
+        <?= Html::img('@web/img/nankai-logo-full.svg', ['alt' => '南开大学', 'class' => 'jumbotron-logo']) ?>
         <h1>南开大学软件学院服务器作业管理平台</h1>
     </div>
 
@@ -17,7 +17,7 @@ $this->title = Yii::$app->name;
             <div class="col-lg-4">
                 <div class="card">
                     <div class="card-header">
-                        <h5 class="card-title">服务器状态查询</h5>
+                        <h5 class="card-title mb-0">服务器状态查询</h5>
                     </div>
                     <div class="card-body">
                         <p class="card-text">浏览服务器状态列表。<br/>查询服务器CPU、内存、GPU等使用情况。</p>
@@ -28,7 +28,7 @@ $this->title = Yii::$app->name;
             <div class="col-lg-4">
                 <div class="card">
                     <div class="card-header">
-                        <h5 class="card-title">服务器作业记录</h5>
+                        <h5 class="card-title mb-0">服务器作业记录</h5>
                     </div>
                     <div class="card-body">
                         <p class="card-text">提交服务器作业记录。<br/>查询所有已提交的服务器作业记录。</p>
@@ -39,7 +39,7 @@ $this->title = Yii::$app->name;
             <div class="col-lg-4">
                 <div class="card">
                     <div class="card-header">
-                        <h5 class="card-title">个人中心</h5>
+                        <h5 class="card-title mb-0">个人中心</h5>
                     </div>
                     <div class="card-body">
                         <p class="card-text">修改个人信息。<br/>查询自己提交的服务器作业记录。</p>
