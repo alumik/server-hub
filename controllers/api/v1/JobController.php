2381c4f41cfd8019012ac8b2083afc760b357fb1
Zhong Zhenyu <nczzy1997@gmail.com>
2022-10-14 17:01:31 +0800

    Add API for register jobs

--- controllers/api/v1/JobController.php
+++ controllers/api/v1/JobController.php
@@ -2,7 +2,10 @@
 
 namespace app\controllers\api\v1;
 
+use app\models\Dictionary;
 use app\models\Job;
+use app\models\Server;
+use app\models\User;
 use Yii;
 use yii\web\Controller;
 use yii\web\Response;
@@ -11,6 +14,7 @@ class JobController extends Controller
 {
     public function beforeAction($action)
     {
+        $this->enableCsrfValidation = false;
         Yii::$app->response->format = Response::FORMAT_JSON;
         return parent::beforeAction($action);
     }
@@ -18,5 +22,34 @@ class JobController extends Controller
     public function actionGpu($server)
     {
         return Job::getGpuJobPid($server);
+    }
+
+    public function actionRegister()
+    {
+        if ($this->request->isPost) {
+            $data = $this->request->post();
+            $student_id = $data['student_id'];
+            $password = $data['password'];
+
+            $user = User::findOne(['student_id' => $student_id]);
+            if ($user && $user->validatePassword($password)) {
+                $job = new Job();
+                $job->description = $data['description'];
+                $job->status = Job::STATUS_ACTIVE;
+                $job->id_server = Server::findOne(['ip' => $data['server_ip']])->id;
+                $job->id_user = $user->id;
+                $job->duration = Dictionary::findOne(['name' => 'job_duration', 'value' => $data['duration']])->key;
+                $job->pid = $data['pid'];
+                $job->server_user = $data['server_user'];
+                $job->comm = $data['command'] ?? '';
+                $job->use_gpu = $data['use_gpu'];
+                if ($job->save()) {
+                    return ['message' => 'success'];
+                } else {
+                    return ['message' => $job->errors];
+                }
+            }
+        }
+        return ['message' => 'authentication failed'];
     }
 }
